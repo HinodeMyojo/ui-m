3f2eb92a66829b18048cb056ae1cc9a94213851c
Egor Semenov <Hinodem@mail.ru>
2026-03-04 06:07:19 +0300

    fix: ⚠ crypto.randomUUID is not a function

--- src/views/pdf-reader/composables/usePdfTranslation.js
+++ src/views/pdf-reader/composables/usePdfTranslation.js
@@ -178,7 +178,7 @@ export function usePdfTranslation(fileName) {
                 'Content-Type': 'application/x-www-form-urlencoded',
                 'Accept': 'application/json',
                 'Authorization': `Basic ${authKey}`,
-                'RqUID': crypto.randomUUID(),
+                'RqUID': (crypto.randomUUID ?? (() => ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c => (c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))).toString(16))))(),
             },
             body: 'scope=GIGACHAT_API_PERS',
         });
